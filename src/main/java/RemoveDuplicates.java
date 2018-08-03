44e7ba3bc74325b94476186b96ca4331334605f1
yestermorrow <yueju@2dfire.com>
2018-08-03 15:07:02 +0800

    给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。

--- src/main/java/RemoveDuplicates.java
+++ src/main/java/RemoveDuplicates.java
@@ -8,8 +8,8 @@ public class RemoveDuplicates {
     public int remove(int[] nums) {
         if (nums.length == 0)
             return 0;
-        int index = 0;
-        for (int i=0; i<=nums.length; i++) {
+        int index = 1;
+        for (int i=0; i<nums.length; i++) {
             if (nums[i] != nums[index]) {
                 index++;
                 nums[index] = nums[i];
