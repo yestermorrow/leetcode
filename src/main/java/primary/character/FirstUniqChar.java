aca23a68e88fc1da94bdafbdda5681fe5bf73167
yueju <yueju@2dfire.com>
2018-10-31 16:35:48 +0800

    给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。

--- src/main/java/primary/character/FirstUniqChar.java
+++ src/main/java/primary/character/FirstUniqChar.java
@@ -1,18 +1,30 @@
-//package primary.character;
-//
-///**
-// * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
-// */
-//public class FirstUniqChar {
-//    public int firstUniqChar(String s) {
-//        if (s.length() == 0)
-//            return -1;
-//        char[] chars = s.toCharArray();
-//        for (int i = 0; i < s.length(); i++) {
-//            for (int j = 1; j < s.length(); j++) {
-//                if (chars[i] == chars[j])
-//
-//            }
-//        }
-//    }
-//}
+package primary.character;
+
+/**
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ */
+public class FirstUniqChar {
+    public int firstUniqChar(String s) {
+        if (s.length() == 0)
+            return -1;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            boolean repeated = false;
+            for (int j = 0; j < s.length(); j++) {
+                if (i != j && chars[i] == chars[j]) {
+                    repeated = true;
+                    break;
+                }
+            }
+            if (!repeated)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "leetleetcode";
+        FirstUniqChar firstUniqChar = new FirstUniqChar();
+        firstUniqChar.firstUniqChar(s);
+    }
+}
